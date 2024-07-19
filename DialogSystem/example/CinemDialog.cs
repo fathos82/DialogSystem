e6cefacc0f9a191149688a00c066eef7855f85ae
Athos Ferreira <fathos82@gmail.com>
2024-07-19 16:37:15 -0300

    Create a test example

---
+++ DialogSystem/example/CinemDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DialogSystem.Properties.example
+{
+    public class CinemDialog: AbstractDialog
+    {
+        public CinemDialog()
+            : base("Qual Filme quer assistir?")
+        {
+            // Permite muito bem ter 2 opções corretas
+            _options.Add(new Option("Dora a aventureira!", true));  
+            _options.Add(new Option("Assasino em Ação!"));   
+            _options.Add(new Option("Nenhum!"));   
+
+        }
+        public override void OnCorrect()
+        {
+            Console.Write("Setting Film Scene...\n");
+        }
+        public override IDialog NextDialog() => new NullDialog();
+
+        
+    }
+
+    
+}
---
+++ DialogSystem/example/InitialDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DialogSystem.Properties.example
+{
+    public class InitialDialog:AbstractDialog
+    {
+        public InitialDialog()
+            : base("Vamos ao Cinema")
+        {
+         _options.Add(new Option("Sim, claro!", true));  
+         _options.Add(new Option("Nao, Obrigado!"));   
+         _options.Add(new Option("Odeio cinema!"));   
+
+        }
+        public override void OnCorrect()
+        {
+            Console.WriteLine("Setting next Scene...\n");
+        }
+        public override IDialog NextDialog() => new CinemDialog();
+
+        
+    }
+
+
+}
